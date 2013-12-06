82a1e75314cf67f9b986e5dd47bc975ee3ba2102
Tomas Martelin <martelit@hotmail.com>
2013-12-06 09:57:56 +0000

    10382 Commit från JazzHub.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello from GitHub");
+		Sy‌stem.out.println("Hello from JazzHub");
+
 	}
 }
 
