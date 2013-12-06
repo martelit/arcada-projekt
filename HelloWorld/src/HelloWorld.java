4ce92d6d017e1d66c5ba402a7924db4598149e7f
martelit <martelit@hotmail.com>
2013-12-06 12:44:55 +0200

    10382 Commit från Eclipse.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello from GitHub");
-		Sy‌stem.out.println("Hello from JazzHub");
-
+		System.out.println("Hello from JazzHub");
+		System.out.println("Hello from Eclipse");
 	}
 }
 
