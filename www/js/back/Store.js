2b92b7d08657c66c8f6eee89f6f6857368eeddac
bollen <mathias.andtbacka@gmail.com>
2014-01-27 15:44:08 +0200

    fixed missing }

--- www/js/back/Store.js
+++ www/js/back/Store.js
@@ -14,60 +14,61 @@
 function Store(){
     (function() {
         var has = function (object, key){
-	    return Object.prototype.hasOwnProperty.call(object, key);
-    };
-	 	
-    var Store = this.Store = function(name, defaults){
-        this.name = name;
-        this.defaults = defaults || {};
-    };
-
-    Store.clear = function () {
-        localStorage.clear();
-    };
-
-    Store.prototype.applyDefaults = function () {
-        for (var key in this.defaults) {
-            if (has(this.defaults, key) && this.get(key) === undefined) {
-                this.set(key, this.defaults[key]);
-            }
-	    }
-
-        return this;
-    };
-
-    Store.prototype.get = function (name) {
-        var value = localStorage.getItem("store." + this.name + "." + name);
-        if (value === null) { return undefined; }
-        try { return JSON.parse(value); } catch (e) { return null; }
-    };
+	        return Object.prototype.hasOwnProperty.call(object, key);
+        };
+    	 	
+        var Store = this.Store = function(name, defaults){
+            this.name = name;
+            this.defaults = defaults || {};
+        };
     
-    Store.prototype.set = function (name, value) {
-        if (value === undefined) {
-            this.remove(name);
-        } else {
-            if (typeof value === "function") { value = null; }
-            try { value = JSON.stringify(value); } catch (e) { value = null; }
-            localStorage.setItem("store." + this.name + "." + name, value);
-        }
+        Store.clear = function () {
+            localStorage.clear();
+        };
+    
+        Store.prototype.applyDefaults = function () {
+            for (var key in this.defaults) {
+                if (has(this.defaults, key) && this.get(key) === undefined) {
+                    this.set(key, this.defaults[key]);
+                }
+    	    }
+    
+            return this;
+        };
+    
+        Store.prototype.get = function (name) {
+            var value = localStorage.getItem("store." + this.name + "." + name);
+            if (value === null) { return undefined; }
+            try { return JSON.parse(value); } catch (e) { return null; }
+        };
         
-        return this;
-    };
-    
-    Store.prototype.remove = function (name) {
-        localStorage.removeItem("store." + this.name + "." + name);
-        return this.applyDefaults();
-    };
-    
-    Store.prototype.reset = function () {
-        var name = "store." + this.name + ".";
-        for (var i = (localStorage.length - 1); i >= 0; i--) {
-            if (localStorage.key(i).substring(0, name.length) === name) {
-                localStorage.removeItem(localStorage.key(i));
+        Store.prototype.set = function (name, value) {
+            if (value === undefined) {
+                this.remove(name);
+            } else {
+                if (typeof value === "function") { value = null; }
+                try { value = JSON.stringify(value); } catch (e) { value = null; }
+                localStorage.setItem("store." + this.name + "." + name, value);
             }
-        }
+            
+            return this;
+        };
         
-        return this.applyDefaults();
-    };
-	
-}());
+        Store.prototype.remove = function (name) {
+            localStorage.removeItem("store." + this.name + "." + name);
+            return this.applyDefaults();
+        };
+        
+        Store.prototype.reset = function () {
+            var name = "store." + this.name + ".";
+            for (var i = (localStorage.length - 1); i >= 0; i--) {
+                if (localStorage.key(i).substring(0, name.length) === name) {
+                    localStorage.removeItem(localStorage.key(i));
+                }
+            }
+            
+            return this.applyDefaults();
+        };
+    	
+    }());
+}
