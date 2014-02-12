df4d004151949a75a222498bc64d6b6f33df198b
Max Gräsbeck <grasbecm@arcada.fi>
2014-02-12 16:51:25 +0200

    phonegap stuff

---
+++ phonegap_extra.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## 
+cp www/icon.png platforms/android/res/drawable/icon.png
+cp www/icon.png platforms/android/res/drawable-ldpi/icon.png
+cp www/icon.png platforms/android/res/drawable-xhdpi/icon.png
+cp www/icon.png platforms/android/res/drawable-hdpi/icon.png
+cp www/icon.png platforms/android/res/drawable-mdpi/icon.png
+
+## http://docs.phonegap.com/en/2.0.0/cordova_media_media.md.html
+## TODO add some way to add this, either a more intelligent gitignore file or some other way
