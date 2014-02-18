9112e17373ffd305967d2a42fb969186589009ed
Jimi Pirilä <jimi.pirila@nordsoftware.com>
2014-02-19 00:31:22 +0200

    Update bonus-version Add bonus-game to require-config

--- www/js/main.js
+++ www/js/main.js
@@ -9,11 +9,15 @@ require.config({
 		sounds:		'front/sounds',
 		helper:		'front/helper',
 		stats:		'front/stats',
-		front:		'front/main'
+		front:		'front/main',
+        phaser:     'bonus-game/src/js/lib/phaser',
+        pixi:       'bonus-game/src/js/lib/pixi',
+        p2:         'bonus-game/src/js/lib/p2',
+        bonus:      'bonus-game/src/js/Bonus'
 	},
 	shim: {
 		'jqm': ['jquery'],
-		'front': ['back','sounds','helper'],
+		'front': ['back','sounds','helper','bonus'],
 		'back': ['generator', 'store'],
 		'helper': ['back']
 	}
