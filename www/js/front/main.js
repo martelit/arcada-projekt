eff7368d4d8951ed012208f861b6b96e5589ba50
Bogezu <bjorn.bergstrom@elisanet.fi>
2014-02-19 08:10:36 +0200

    Indented main.js

--- www/js/front/main.js
+++ www/js/front/main.js
@@ -5,6 +5,7 @@ function getRewardData(){
 		canPlay: false
 	};
 }
+
 function setRewardData(data){
 	// TODO do we need this?
 }
@@ -22,7 +23,7 @@ function guessAnswer(guess){
 }
 
 function getLamp(){
-return back.getLampSize();
+	return back.getLampSize();
 }
 
 function getSettings(){
@@ -42,6 +43,7 @@ function getSettings(){
 	//return back.store.get("snille"); ..or
 	return back.getSettings();
 }
+
 function setSettings(){
 	//var store = Backend.settings.set("Settings", settings);
 	
@@ -80,9 +82,11 @@ $(document).on('pageshow', function(){
 			"title",
 			"Button text"
 		);
-	} else {
+	}
+	else {
 		console.log("Not phonegap");
 	}
+	
 //Starts and stops music
 $( ".flip-music" ).change(function () {
 	if(this.value=="on")
@@ -98,6 +102,7 @@ $( ".flip-music" ).change(function () {
 });
 			
 	var Sound;
+	
 $( ".flip-sound" ).change(function () {
 	if(this.value=="on")
 	{
@@ -108,37 +113,45 @@ $( ".flip-sound" ).change(function () {
 		Sound=false;
 	}
 });
+
 	//plays a click sound
-	$(".play-click").click(function(){
-		if(Sound)
-		{
-			play("click");
-		}
-		else{}
-	});
+$(".play-click").click(function(){
+	if(Sound)
+	{
+		play("click");
+	}
+	else
+	{
+		
+	}
+});
+
 	//starts playing background music
-	$(".play-music").click(function(){
-		music.playclip();
-	});
+$(".play-music").click(function(){
+	music.playclip();
+});
+
 	//pauses background music
-	$(".stop-music").click(function(){
-		music.pause();
-	});
+$(".stop-music").click(function(){
+	music.pause();
+});
 	
 	var task = getQuestion();
-	$('#question-holder').html(task.question);
+	
+$('#question-holder').html(task.question);
 
-	$('.button-container').find('button').each(function(i){
-		$(this).html(task.answers[i]);
-	});
+$('.button-container').find('button').each(function(i){
+	$(this).html(task.answers[i]);
+});
+
 	var displayQuestion = task.question;
-	$(".answer-button").click(function(){
-		//problem, always sets 2 questions_answered when one is answered? -Bogezu
-		//getAnswer is called twice. once here, once in helper. -Neko
-		console.log("total questions:"+back.questions_answered);
-		console.log("bonus available: "+back.bonusIsAvailable());
-		var answer = $(this).html();
-		var correctAnswer = back.getAnswer();
+$(".answer-button").click(function(){
+	//problem, always sets 2 questions_answered when one is answered? -Bogezu
+	//getAnswer is called twice. once here, once in helper. -Neko
+	console.log("total questions:"+back.questions_answered);
+	console.log("bonus available: "+back.bonusIsAvailable());
+	var answer = $(this).html();
+	var correctAnswer = back.getAnswer();
 		if( guessAnswer(answer) ){
 			$(this).addClass("guessed-answer correct-answer");
 			//$("#correct-wrong").text("Du svarade rätt!");
@@ -151,7 +164,9 @@ $( ".flip-sound" ).change(function () {
 			displayQuestion = displayQuestion.replace(",", " ");
 			displayQuestion = displayQuestion.replace(",", " ");
 			$("#correct-answer-number").text(displayQuestion +" = "+ correctAnswer);
-		} else {
+		}
+		else
+		{
 			$(this).addClass("guessed-answer wrong-answer");
 			//$("#correct-wrong").text("Du svarade fel.");
 			//$("#correct-answer-number").text(correctAnswer);
@@ -164,37 +179,37 @@ $( ".flip-sound" ).change(function () {
 			displayQuestion = displayQuestion.replace(",", " ");
 			$("#correct-answer-number").text(displayQuestion +" = "+ correctAnswer);
 		}
-		$("#response-popup").popup("open");
-	});
+	$("#response-popup").popup("open");
+});
 	
-		$(".next-button").click(function(){
-		
-		var nextTask = getQuestion();		
-		displayQuestion = nextTask.question;
-		$('#question-holder').html(nextTask.question);
+$(".next-button").click(function(){		
+	var nextTask = getQuestion();		
+	displayQuestion = nextTask.question;
+	$('#question-holder').html(nextTask.question);
 		$('.button-container').find('button').each(function(i){
-		$(this).html(nextTask.answers[i]);
+			$(this).html(nextTask.answers[i]);
 		});
-		$("#response-popup").popup("close");
-		});
+	$("#response-popup").popup("close");
+});
 		
-		
-		$(".bonus-button").click(function(){
+$(".bonus-button").click(function(){
 		if(back.bonusIsAvailable() == true){
-		//start bonus game
-		$.mobile.changePage("#rewards");
-		$("#response-popup").popup("close");
-		}else{
-		//Next question
-		var nextTask = getQuestion();
-		displayQuestion = nextTask.question;
-		$('#question-holder').html(nextTask.question);
-		$('.button-container').find('button').each(function(i){
-		$(this).html(nextTask.answers[i]);
-		});
+			//start bonus game
+			$.mobile.changePage("#rewards");
+			$("#response-popup").popup("close");
+		}
+		else
+		{
+			//Next question
+			var nextTask = getQuestion();
+			displayQuestion = nextTask.question;
+			$('#question-holder').html(nextTask.question);
+				$('.button-container').find('button').each(function(i){
+					$(this).html(nextTask.answers[i]);
+				});
 		$("#response-popup").popup("close");
 		}
-		});
+});
 
 	// Sets min and max values for the sliders.
 	// This is is not optimal, but probably the best solution if we want to
@@ -217,33 +232,33 @@ $( ".flip-sound" ).change(function () {
 		}
 	} );
 	*/
-	//Deselects all other arithmetic options if symbols are selected (counting without arithmetic)
-	$("#symbols").click(function(){
-		$(this).closest('#arithmetic-settings').find('input[type="checkbox"]:not("#symbols")').prop('checked', false).checkboxradio( "refresh" );
-	});
 	
-	//Deselects symbols (counting without arithmetic) if any other arithmetic setting is clicked
-	$('#arithmetic-settings input[type="checkbox"]:not("#symbols")').click(function(){
+	//Deselects all other arithmetic options if symbols are selected (counting without arithmetic)
+$("#symbols").click(function(){
+	$(this).closest('#arithmetic-settings').find('input[type="checkbox"]:not("#symbols")').prop('checked', false).checkboxradio( "refresh" );
+});
+	
+//Deselects symbols (counting without arithmetic) if any other arithmetic setting is clicked
+$('#arithmetic-settings input[type="checkbox"]:not("#symbols")').click(function(){
 		if($('#symbols').prop('checked', true)){
 			$("#symbols").prop('checked', false).checkboxradio( "refresh" );
 		}
-	});
+});
 	
 	
 	
 	// TODO doesn't get settings on second click, check why and fix or make this happen on settings page load
-	$("#settingsBtn").click(function(){
-		var settings = getSettings();
-		$('#min').attr('value',settings.smallestNumber);
-		$('#max').attr('value',settings.greatestNumber);
-		$('#addition').attr('value',settings.addition);
-		$('#subtraction').attr('value',settings.subtraction);
-		$('#multiplication').attr('value',settings.multiplication);
-		$('#division').attr('value',settings.disivion);
-		$('#symbols').attr('value', settings.symbols);
-		$('#bonus').attr('value', settings.bonus);
-		// TODO Backend needs to take in the new values (symbols & bonus) and set the amount of questions needed answered before bonusIsAvailable() becomes true to the new value. -Bogezu
-		// TODO change the rest of the settings page to what the settings are
-	});
-	
+$("#settingsBtn").click(function(){
+	var settings = getSettings();
+	$('#min').attr('value',settings.smallestNumber);
+	$('#max').attr('value',settings.greatestNumber);
+	$('#addition').attr('value',settings.addition);
+	$('#subtraction').attr('value',settings.subtraction);
+	$('#multiplication').attr('value',settings.multiplication);
+	$('#division').attr('value',settings.disivion);
+	$('#symbols').attr('value', settings.symbols);
+	$('#bonus').attr('value', settings.bonus);
+	// TODO Backend needs to take in the new values (symbols & bonus) and set the amount of questions needed answered before bonusIsAvailable() becomes true to the new value. -Bogezu
+	// TODO change the rest of the settings page to what the settings are
+});	
 });
