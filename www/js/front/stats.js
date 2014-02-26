dc243fadf95d007e91a4a9ae4f44cf509b8a892d
johnrothberg <rothberg.jo@gmail.com>
2014-02-26 15:16:15 +0200

    Stats update /js/front/stats.js Column amount 4 --> 5

--- www/js/front/stats.js
+++ www/js/front/stats.js
@@ -2,7 +2,7 @@ function drawStats(){
 	console.log("hello from drawStats");
 	var stats = '';
 	
-	for(var cols = 0; cols < 4; cols++){
+	for(var cols = 0; cols < 5; cols++){
 		stats = stats + '<div class="stats-col">';
 		for(var rows = 10; rows > 0; rows--){
 			stats = stats + '<div class="stats-row">' + rows + "</div>"
@@ -12,5 +12,5 @@ function drawStats(){
 	
 	$('.stats-draw').html(stats);
 	$('.stats-draw').parent().height($(window).height() );
-	$('.stats-row').height($(window).height()/10);
+	$('.stats-row').height($(window).height()/11);
 }
